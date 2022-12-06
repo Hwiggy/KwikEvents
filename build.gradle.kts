af81a35592a8b940ee22d05adda0f70f3281f0b2
Hunter Wignall <Hwiggy@users.noreply.github.com>
2022-12-05 22:32:31 -0500

    Add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.Properties
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    `maven-publish`
+}
+
+val properties = Properties().also {
+    val localFile = rootProject.file("local.properties")
+    if (!localFile.exists()) return@also
+    it.load(localFile.reader())
 }
 
 group = "me.hwiggy"
-version = "1.0"
+version = "1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -16,6 +23,33 @@ dependencies {
     implementation("com.google.guava:guava:31.1-jre")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            val baseUrl = "https://nexus.mcdevs.us/repository"
+            if (project.version.toString().endsWith("-SNAPSHOT")) {
+                setUrl("${baseUrl}/mcdevs-snapshots/")
+                mavenContent { snapshotsOnly() }
+            } else {
+                setUrl("${baseUrl}/mcdevs-releases/")
+                mavenContent { releasesOnly() }
+            }
+            credentials {
+                username = properties["NEXUS_USERNAME"] as String?
+                password = properties["NEXUS_PASSWORD"] as String?
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("assembly") {
+            from(components["java"])
+        }
+    }
 }
