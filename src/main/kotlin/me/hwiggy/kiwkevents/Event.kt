ed8bd7280ded9c2c5313326523a9b05e12610cdd
Hunter Wignall <Hwiggy@users.noreply.github.com>
2022-12-05 22:54:43 -0500

    Fuck it anything can be an event now

--- src/main/kotlin/me/hwiggy/kiwkevents/Event.kt
+++ src/main/kotlin/me/hwiggy/kiwkevents/Event.kt
@@ -18,7 +18,7 @@ interface Event {
     /**
      * Executor of all incoming event logic
      */
-    interface Handler<TEvent : Event> : (TEvent) -> Unit {
+    interface Handler<TEvent> : (TEvent) -> Unit {
         fun priority() = Priority.NORMAL
         fun ignoreCancelled() = false
     }
@@ -26,7 +26,7 @@ interface Event {
     /**
      * Responsible for dispatching events to the relevant handlers, as well as maintaining [Subscription]s
      */
-    @Suppress("UNCHECKED_CAST") open class Transport {
+    @Suppress("UNCHECKED_CAST") open class Transport<TEvent : Any> {
         private val participants = Multimaps.newSetMultimap<Participant, Closeable>(IdentityHashMap(), ::HashSet)
         private val listeners: Multimap<Class<*>, Handler<*>> = Multimaps.newSetMultimap(
             IdentityHashMap()
@@ -35,7 +35,7 @@ interface Event {
         /**
          * Submits an event to the [Transport], invoking the [Handler]s in order of their [Priority]
          */
-        fun <TEvent : Event> submit(event: TEvent) {
+        fun submit(event: TEvent) {
             if (!shouldAccept(event)) return
             val type = event::class.java
             val handlers = (listeners[type] ?: return) as Collection<Handler<TEvent>>
@@ -67,7 +67,7 @@ interface Event {
          * Subscribes to events incoming to the [Transport] using a custom [Handler] implementation
          * @see [Participant] for a means of keeping track of returned [Subscription]s
          */
-        fun <TEvent : Event> subscribe(type: Class<TEvent>, handler: Handler<TEvent>): Subscription {
+        fun <REvent : TEvent> subscribe(type: Class<REvent>, handler: Handler<REvent>): Subscription {
             listeners.put(type, handler)
             return Closeable { listeners.remove(type, handler) }
         }
@@ -76,43 +76,43 @@ interface Event {
          * @see subscribe(Class, Handler)
          * @see [Participant] for a means of keeping track of returned [Subscription]s
          */
-        inline fun <reified TEvent : Event> subscribe(handler: Handler<TEvent>) = subscribe(TEvent::class.java, handler)
+        inline fun <reified REvent : TEvent> subscribe(handler: Handler<REvent>) = subscribe(REvent::class.java, handler)
 
         /**
          * Subscribes to events incoming to the [Transport] using an anonymous [Handler] implementation
          * @see [Participant] for a means of keeping track of returned [Subscription]s
          */
-        fun <TEvent : Event> subscribe(
-            type: Class<TEvent>,
+        fun <REvent : TEvent> subscribe(
+            type: Class<REvent>,
             priority: Priority = Priority.NORMAL,
             ignoreCancelled: Boolean = false,
-            block: (TEvent) -> Unit
-        ): Subscription = subscribe(type, object : Handler<TEvent> {
+            block: (REvent) -> Unit
+        ): Subscription = subscribe(type, object : Handler<REvent> {
             override fun priority() = priority
             override fun ignoreCancelled() = ignoreCancelled
-            override fun invoke(p1: TEvent) = block(p1)
+            override fun invoke(p1: REvent) = block(p1)
         })
 
         /**
          * @see subscribe(Class, Priority, Boolean, Consumer<TEvent>)
          * @see [Participant] for a means of keeping track of returned [Subscription]s
          */
-        inline fun <reified TEvent : Event> subscribe(
+        inline fun <reified REvent : TEvent> subscribe(
             priority: Priority = Priority.NORMAL,
             ignoreCancelled: Boolean = false,
-            noinline block: (TEvent) -> Unit
-        ) = subscribe(TEvent::class.java, priority, ignoreCancelled, block)
+            noinline block: (REvent) -> Unit
+        ) = subscribe(REvent::class.java, priority, ignoreCancelled, block)
 
         /**
          * Whether this [Transport] should accept a specific [TEvent]
          */
-        open fun <TEvent : Event> shouldAccept(event: TEvent): Boolean = true
+        open fun shouldAccept(event: TEvent): Boolean = true
     }
 
     /**
      * The global [Transport], accepts all [Event]s indiscriminately
      */
-    object GlobalTransport : Transport()
+    object GlobalTransport : Transport<Event>()
 
     /**
      * An object which provides [Subscription]s to specific [Event]s
